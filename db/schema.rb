da221608ba448442ab90bf588bc51c9d0b5c65f9
kei-60th <kei-1005@i.softbank.jp>
2019-10-14 15:40:51 +0900

    データベース誕生日のヌルを許可（登録できるようにするため）

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2019_10_10_042949) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
+    t.integer "birth_year"
+    t.integer "birth_month"
+    t.integer "birth_day"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
