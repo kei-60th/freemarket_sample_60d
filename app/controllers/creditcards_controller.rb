17e7ed12811e73995cd1ae9fbe3d512f4c15907f
kei-60th <kei-1005@i.softbank.jp>
2019-10-15 13:18:28 +0900

    カード情報の登録処理

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -4,7 +4,17 @@ class CreditcardsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @creditcard= Creditcard.new(create_params)
+    if @creditcard.save
+
+    else
+      render :new
+    end
+  end
+
+  private
+  def create_params
+    params.require(:creditcard).permit(:curd_number, :cvc).merge(user_id: User.last.id,valid_year: params[:valid_year], valid_month:params[:valid_month])
   end
 
 
