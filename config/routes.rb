c56228aba676ccb6008394a0e272e91e7deb2337
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-15 19:15:19 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,6 @@ get '/mypage/card', to: 'mypage#index'
 get '/mypage/card/create', to: 'mypage#create'
 get '/registration', to: 'registrations#index'
 get '/registration/new', to: 'sessions#new'
-get '/login', to: 'users#new'
 get "/registration/compleate", to: "compleate#edit"
 
 end
