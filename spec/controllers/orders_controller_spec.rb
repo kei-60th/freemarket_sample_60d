d605f98da5586b9c4b1c71e4c4f7e3b1bef83ffd
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-18 20:00:22 +0900

    コメントアウトの削除

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -7,11 +7,5 @@ describe OrdersController, type: :controller do
       get :show, params: { item_id: 1 }
       expect(response).to render_template :show
     end
-
-    # it "renders the :create template" do
-    #   @item = create(:item)
-    #   get :create, params: { item_id: 1, 'payjp-token': "tok_58cfc982c1f16344552c266a0cae" }
-    #   expect(response).to render_template :create
-    # end
   end
 end
