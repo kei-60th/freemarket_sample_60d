b224d02d94140674d13b5477ef88d03f2934fffc
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-18 19:50:58 +0900

    テストコード記述編集

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe OrdersController, type: :controller do
   describe 'GET #show' do
-
     it "renders the :show template" do
       item = create(:item)
       get :show, params: { item_id: 1 }
       expect(response).to render_template :show
     end
+
+    # it "renders the :create template" do
+    #   @item = create(:item)
+    #   get :create, params: { item_id: 1, 'payjp-token': "tok_58cfc982c1f16344552c266a0cae" }
+    #   expect(response).to render_template :create
+    # end
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :item do
     id {1}
     name {"アイテム"}
-    description {"商品説明欄"}
     price {"100000"}
+    description {"商品説明欄"}
+    order_status {1}
+    category {1}
+    size {1}
+    user_id {1}
   end
 end
