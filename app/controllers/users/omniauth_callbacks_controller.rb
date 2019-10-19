c3dcbfd34c6c3447171abe80a4bd696d2dd88387
dancingflower19 <myka0109@gmail.com>
2019-10-19 13:41:12 +0900

    facebookログイン機能実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+    callback_for(:facebook) #コールバック
+  end
+
+  def callback_for(provider)
+    info = User.find_oauth(request.env["omniauth.auth"]) #usersモデルのメソッド
+    @user = info[:user]
+    sns_id = info[:sns_id]
+    # binding.pry
+    if @user.persisted? #userが存在したら
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else #userが存在しなかったら
+      session["devise.sns_id"] = sns_id #sns_credentialのid devise.他のアクションに持ち越せる(少し難)
+      render template: "devise/registrations/new"　#redirect_to だと更新してしまうのでrenderで
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,22 +16,49 @@ class User < ApplicationRecord
   # has_one :evaluation
   # belongs_to :prefecture
 
-  # userがいなければfacebookアカウントでuserを作成するメソッド
-  def self.from_omniauth(auth)
-    user = User.where(email: auth.info.email).first
-    if user
-      return user
-    else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-        user.name = auth.info.name
-        user.image = auth.info.image
-        user.uid = auth.uid
-        user.provider = auth.provider
-        user.skip_confirmation!
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # binding.pry
+
+    if snscredential.present? #sns登録のみ完了してるユーザー
+      user = User.where(id: snscredential.user_id).first
+      unless user.present? #ユーザーが存在しないなら
+        user = User.new(
+          # snsの情報
+          # binding.pry => auth.infoとかで確認
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+      #binding.pry
+
+    else #sns登録 未
+      user = User.where(email: auth.info.email).first
+      if user.present? #会員登録 済
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else #会員登録 未
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+        # binding.pry
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+        # binding.pry 
       end
     end
+    # binding.pry
+    # hashでsnsのidを返り値として保持しておく
+    return { user: user , sns_id: sns.id }
   end
 
   validates :nickname, presence: true
