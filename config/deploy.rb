38516417f3599d2d2b107a573dcb8d3be81c4c1e
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-19 14:51:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,14 @@ set :repo_url, 'git@github.com:kei-60th/freemarket_sample_60d.git'
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+#capistranoが環境変数を参照できるようにする
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: Rails.application.credentials.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.AWS_SECRET_ACCESS_KEY
+}
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
@@ -55,9 +63,22 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
+set :linked_files, %w{ config/secrets.yml }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
