06fa83619b1c7812f95262118d62a64f12ba3843
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-19 15:21:57 +0900

    set environment variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  if Rails.env.production?
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.AWS_SECRET_ACCESS_KEY,
-      aws_secret_access_key: Rails.application.secrets.AWS_ACCESS_KEY_ID,
-      region: 'ap-northeast-1'
-    }
-  end
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
+    region: 'ap-northeast-1'
+  }
 
   config.fog_directory  = 'mercari60d'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari60d'
