294e8b02e32b7cff055b11a6afd04b932c2e7219
kurashi293 <my_post_office@icloud.com>
2019-10-20 13:31:23 +0900

    マイページビューの微修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "/login", :to => "users/sessions#new"
+    get "/registration/information", :to => "users/registrations#new"
+    post "/registration/information/create", :to => "users/registrations#create"
+  end
+
+resources :addresses, only: [:create]
+resources :creditcards, only: [:new, :create]
+resources :items, only: [:new, :create, :index, :show]
+
+root "items#index"
+
+get '/toppage', to: 'test#index'
+get '/signin', to: 'users#new'
+get '/logout', to: 'mypage#logout'
+get '/registration', to: 'registration#confirmation'
+get "/registration/confirmation", to: "sms_confirmation#edit"
+get "/registration/address", to: "addresses#new"
+
+get '/mypage', to: 'mypage#show'
+get '/mypage/card', to: 'mypage#index'
+get '/mypage/card/create', to: 'mypage#create'
+get '/registration', to: 'registrations#index'
+get '/registration/new', to: 'sessions#new'
+get "/registration/compleate", to: "compleate#edit"
+
+get "/item", to: "item_details#index"
+
+get "/privacypolicy", to: 'privacypolicy#index'
+
+resources :items do
+  get "/order/confirmation", to: "orders#show"
+  post "/order/confirmation", to: "orders#create"
+>>>>>>> Stashed changes
 end
