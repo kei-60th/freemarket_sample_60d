c9f75c5456158e846efddc126c0a5d629f99116e
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-20 18:32:28 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,38 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :nickname, uniqueness: true
-  validates :last_name, presence: true, format: { with: /\A[^a-zA-Z0-9]+\z/,
-    message: "全角文字で入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[^a-zA-Z0-9]+\z/,
-    message: "全角文字で入力してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴ]+\z/,
-    message: "全角カタカナで入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴ]+\z/,
-    message: "全角カタカナで入力してください" }
+  validates :last_name, 
+    presence: true,
+    format: {
+      with: /\A[^a-zA-Z0-9]+\z/,
+      message: "全角文字で入力してください"
+    }
+  validates :first_name,
+    presence: true,
+    format: {
+      with: /\A[^a-zA-Z0-9]+\z/,
+      message: "全角文字で入力してください"
+    }
+  validates :last_name_kana,
+    presence: true,
+    format: {
+      with: /\A[ァ-ヴ]+\z/,
+      message: "全角カタカナで入力してください"
+    }
+  validates :first_name_kana,
+    presence: true,
+    format: {
+      with: /\A[ァ-ヴ]+\z/,
+      message: "全角カタカナで入力してください"
+    }
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :password, presence: true, length: { minimum: 7, too_short: "パスワードは7文字以上です" }
+  validates :password,
+    presence: true,
+    length: {
+      minimum: 7,
+      too_short: "パスワードは7文字以上です"
+    }
   validates :birth_year, presence: true
   validates :birth_month, presence: true
   validates :birth_day, presence: true
