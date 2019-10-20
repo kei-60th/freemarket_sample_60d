89d0fc6bcc696e78df201371c4f3243f3c337d70
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-20 11:52:25 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,8 +22,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.AWS_SECRET_ACCESS_KEY,
-      aws_secret_access_key: Rails.application.credentials.AWS_ACCESS_KEY_ID,
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'mercari60d'
