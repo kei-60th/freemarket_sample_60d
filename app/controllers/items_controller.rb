0b18e4a3aab9b85bd1af7c0ec8c3ad73c79266b7
kurashi293 <my_post_office@icloud.com>
2019-10-21 06:51:33 +0900

    ログイン機能のアップデート

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
 
   def index
     @items = Item.all.includes(:item_images).limit(5).order("created_at DESC")
