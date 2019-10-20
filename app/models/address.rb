bd97e769391b17db37662c4161afc098f8021ea7
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-20 19:37:25 +0900

    addressのバリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,8 +2,26 @@ class Address < ApplicationRecord
   belongs_to :user
   has_one :prefecture
 
-  validates :postcode, presence: true
-  validates :city,     presence: true
-  validates :address,  presence: true
-  validates :user_id,  presence: true
+  validates :postcode,
+    presence: {
+      message: "郵便番号を入力してください"
+    },
+    format: {
+      with: /\A\d{3}-\d{4}\z/,
+      message: "数字とハイフンの位置は正しく入力してください"
+    }
+  validates :city,     
+    presence: {
+      message: "市区町村を入力してください"
+    },
+    format: {
+      with: /\A[^a-zA-Z0-9]+\z/,
+      message: "全角文字で入力してください"
+    }
+  validates :address,
+    presence: {
+      message: "番地を入力してください"
+    }
+  validates :user_id,
+    presence: true
 end
