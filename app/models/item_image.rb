ab7e7dc038e66d09bd06683ddbe186fb7112e21f
kei-60th <kei-1005@i.softbank.jp>
2019-10-22 15:34:02 +0900

    バリデーションを一時コメントアウト

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,7 +2,7 @@ class ItemImage < ApplicationRecord
   belongs_to :item
   mount_uploader :image, ImageUploader
 
-  validates :image, 
-    presence: { comment: "画像がありません"}
-  validates :item_id, presence: true
+  # validates :image, 
+  #   presence: { comment: "画像がありません"}
+  # validates :item_id, presence: true
 end
