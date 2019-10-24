99b9afc164de50dfaed5f993745c67292168dc3e
kurashi293 <my_post_office@icloud.com>
2019-10-24 12:34:01 +0900

    商品詳細　画像の切り替え、ビューの修正

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,7 @@
+$(function(){
+  // 「imageList」内の「img」をマウスオーバーした場合
+  $('.left-bottom__array img').hover(function(){
+  // マウスオーバーしている画像をメインの画像に反映
+  $('.left-top img').attr('src', $(this).attr('src'));
+  });
+});
