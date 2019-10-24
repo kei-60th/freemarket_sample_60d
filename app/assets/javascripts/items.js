63837b816822d7d0f32d6e7e8477929e43575c32
kurashi293 <my_post_office@icloud.com>
2019-10-24 18:54:14 +0900

    一旦マージします

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,5 @@
 $(function(){
-  // 「imageList」内の「img」をマウスオーバーした場合
+  // 「left-bottom__array」内の「img」をマウスオーバーした場合
   $('.left-bottom__array img').hover(function(){
   // マウスオーバーしている画像をメインの画像に反映
   $('.left-top img').attr('src', $(this).attr('src'));
