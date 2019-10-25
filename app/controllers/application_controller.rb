eb9c181f2ce4a3d079b94678d71329b33f68bc04
kei-60th <kei-1005@i.softbank.jp>
2019-10-25 16:48:13 +0900

    ヘッダーにもカテゴリバー機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_category_list
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :birth_year, :birth_month, :birth_day])
+  end
+
+  def set_category_list
+    @category_list = Category.all
   end
 end
