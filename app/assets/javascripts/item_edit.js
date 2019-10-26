302b08a9319c90d218f6502ad0156a746144ee11
KatsunoriGunji <l.hvokj@gmail.com>
2019-10-26 15:22:10 +0900

    賞品出品・編集ページのJQuery修正

--- app/assets/javascripts/item_edit.js
+++ app/assets/javascripts/item_edit.js
@@ -1,28 +1,28 @@
-function appendProfit(priceForm, feeForm, profitForm){
-  var price = $(priceForm).val();
-  var fee = Math.round(price * 0.1);
-  var profit = price - fee;
-  $(feeForm).empty();
-  $(profitForm).empty();
-  $(feeForm).append("¥" + fee.toLocaleString());
-  $(profitForm).append("¥" + profit.toLocaleString());
-};
+$(document).on('turbolinks:load', function() {
+  function appendProfit(priceForm, feeForm, profitForm){
+    var price = $(priceForm).val();
+    var fee = Math.round(price * 0.1);
+    var profit = price - fee;
+    $(feeForm).empty();
+    $(profitForm).empty();
+    $(feeForm).append("¥" + fee.toLocaleString());
+    $(profitForm).append("¥" + profit.toLocaleString());
+  };
 
-function appendHyphen(feeForm, profitForm){
-  $(feeForm).empty();
-  $(profitForm).empty();
-  $(feeForm).append("-");
-  $(profitForm).append("-");
-};
+  function appendHyphen(feeForm, profitForm){
+    $(feeForm).empty();
+    $(profitForm).empty();
+    $(feeForm).append("-");
+    $(profitForm).append("-");
+  };
 
-$(function(){
   var priceForm = $('#item-edit-price')[0];
   var feeForm =  $("#item-edit-fee")[0];
   var profitForm = $("#item-edit-profit")[0];
 
   appendProfit(priceForm, feeForm, profitForm);
 
-  $(document).on("keyup", '#item-edit-price', function(){
+  $(priceForm).on("keyup", function(){
     price = $(priceForm).val();
     if (price>=300 && price<=9999999) {
       appendProfit(priceForm, feeForm, profitForm);
@@ -31,58 +31,58 @@ $(function(){
       appendHyphen(feeForm, profitForm)
     }
   });
-});
 
-$(document).on("change", '#item-edit-select-box2', function(){
-  var GrandParentCategoryId = $('#item_category_id').val();
-  var selectBox = $('#item-edit-select-box2');
-  var categoryId = $(selectBox).val();
+  $(document).on("change", '#item-edit-select-box2', function(){
+    var GrandParentCategoryId = $('#item_category_id').val();
+    var selectBox = $('#item-edit-select-box2');
+    var categoryId = $(selectBox).val();
 
-  $.ajax({
-    type: 'GET',
-    url: '/item/category_children',
-    data: {categoryId: `${GrandParentCategoryId}/${categoryId}`},
-    dataType: 'json'
-  })
-  .done(function(categories){
-    var options = '';
-    $.each(categories.childrens, function(i){
-      category = categories.childrens[i];
-      options += `<option value=${category.id}>${category.name}</option>`;
-    });
-    $("#item-edit-select-box3").remove();
-    $(selectBox).after(
-      `<select class="item_select-box" id="item-edit-select-box3" name="item[category_id]">
-        <option value="">---</option>
-        ${options}
-      </select>`
-    );
-  })
-});
+    $.ajax({
+      type: 'GET',
+      url: '/item/category_children',
+      data: {categoryId: `${GrandParentCategoryId}/${categoryId}`},
+      dataType: 'json'
+    })
+    .done(function(categories){
+      var options = '';
+      $.each(categories.childrens, function(i){
+        category = categories.childrens[i];
+        options += `<option value=${category.id}>${category.name}</option>`;
+      });
+      $("#item-edit-select-box3").remove();
+      $(selectBox).after(
+        `<select class="item_select-box" id="item-edit-select-box3" name="item[category_id]">
+          <option value="">---</option>
+          ${options}
+        </select>`
+      );
+    })
+  });
 
-$(document).on("change", '#item_category_id', function(){
-  var selectBox = $('#item_category_id');
-  var categoryId = $(selectBox).val();
+  $(document).on("change", '#item_category_id', function(){
+    var selectBox = $('#item_category_id');
+    var categoryId = $(selectBox).val();
 
-  $.ajax({
-    type: 'GET',
-    url: '/item/category_children',
-    data: {categoryId: categoryId},
-    dataType: 'json'
-  })
-  .done(function(categories){
-    var options = '';
-    $.each(categories.childrens, function(i){
-      category = categories.childrens[i];
-      options += `<option value="${category.id}">${category.name}</option>`;
-    });
-    $("#item-edit-select-box3").remove();
-    $("#item-edit-select-box2").remove();
-    $(selectBox).after(
-      `<select class="item_select-box" id="item-edit-select-box2" name="item[parent_id]">
-        <option value="">---</option>
-        ${options}
-      </select>`
-    );
-  })
+    $.ajax({
+      type: 'GET',
+      url: '/item/category_children',
+      data: {categoryId: categoryId},
+      dataType: 'json'
+    })
+    .done(function(categories){
+      var options = '';
+      $.each(categories.childrens, function(i){
+        category = categories.childrens[i];
+        options += `<option value="${category.id}">${category.name}</option>`;
+      });
+      $("#item-edit-select-box3").remove();
+      $("#item-edit-select-box2").remove();
+      $(selectBox).after(
+        `<select class="item_select-box" id="item-edit-select-box2" name="item[parent_id]">
+          <option value="">---</option>
+          ${options}
+        </select>`
+      );
+    })
+  });
 });
--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -1,85 +1,86 @@
-function appendProfit(priceForm, feeForm, profitForm){
-  var price = $(priceForm).val();
-  var fee = Math.round(price * 0.1);
-  var profit = price - fee;
-  $(feeForm).empty();
-  $(profitForm).empty();
-  $(feeForm).append("¥" + fee.toLocaleString());
-  $(profitForm).append("¥" + profit.toLocaleString());
-};
+$(document).on('turbolinks:load', function() {
+  function appendProfit(priceForm, feeForm, profitForm){
+    var price = $(priceForm).val();
+    var fee = Math.round(price * 0.1);
+    var profit = price - fee;
+    $(feeForm).empty();
+    $(profitForm).empty();
+    $(feeForm).append("¥" + fee.toLocaleString());
+    $(profitForm).append("¥" + profit.toLocaleString());
+  };
 
-function appendHyphen(feeForm, profitForm){
-  $(feeForm).empty();
-  $(profitForm).empty();
-  $(feeForm).append("-");
-  $(profitForm).append("-");
-};
+  function appendHyphen(feeForm, profitForm){
+    $(feeForm).empty();
+    $(profitForm).empty();
+    $(feeForm).append("-");
+    $(profitForm).append("-");
+  };
 
-
-$(document).on("keyup", '.item-price_field', function(){
   var priceForm = $('.item-price_field')[0];
-  var feeForm = $("#item-new-fee")[0];
-  var profitForm = $("#item-new-profit")[0];
-  var price = $(priceForm).val();
-  if (price>=300 && price<=9999999) {
-    appendProfit(priceForm, feeForm, profitForm)
-  }
-  else {
-    appendHyphen(feeForm, profitForm)
-  }
-});
+  $(priceForm).on("keyup", function(){
+    var feeForm = $("#item-new-fee")[0];
+    var profitForm = $("#item-new-profit")[0];
+    var price = $(priceForm).val();
+    if (price>=300 && price<=9999999) {
+      appendProfit(priceForm, feeForm, profitForm)
+    }
+    else {
+      appendHyphen(feeForm, profitForm)
+    }
+  });
 
 
-$(document).on("change", '#item_select-box2', function(){
-  var GrandParentCategoryId = $('#item_select-box1').val();
-  var selectBox = $('#item_select-box2');
-  var categoryId = $(selectBox).val();
+  $(document).on("change", '#item_select-box2', function(){
+    var GrandParentCategoryId = $('#item_select-box1').val();
+    var selectBox = $('#item_select-box2');
+    var categoryId = $(selectBox).val();
 
-  $.ajax({
-    type: 'GET',
-    url: '/item/category_children',
-    data: {categoryId: `${GrandParentCategoryId}/${categoryId}`},
-    dataType: 'json'
-  })
-  .done(function(categories){
-    var options = '';
-    $.each(categories.childrens, function(i){
-      category = categories.childrens[i];
-      options += `<option value=${category.id}>${category.name}</option>`;
-    });
-    $("#item_select-box3").remove();
-    $(selectBox).after(
-      `<select class="item_select-box" id="item_select-box3" name="item[category_id]">
-        <option value="">---</option>
-        ${options}
-      </select>`
-    );
-  })
-});
+    $.ajax({
+      type: 'GET',
+      url: '/item/category_children',
+      data: {categoryId: `${GrandParentCategoryId}/${categoryId}`},
+      dataType: 'json'
+    })
+    .done(function(categories){
+      var options = '';
+      $.each(categories.childrens, function(i){
+        category = categories.childrens[i];
+        options += `<option value=${category.id}>${category.name}</option>`;
+      });
+      $("#item_select-box3").remove();
+      $(selectBox).after(
+        `<select class="item_select-box" id="item_select-box3" name="item[category_id]">
+          <option value="">---</option>
+          ${options}
+        </select>`
+      );
+    })
+  });
 
-$(document).on("change", '#item_select-box1', function(){
-  var selectBox = $('#item_select-box1');
-  var categoryId = $(selectBox).val();
+  $(document).on("change", '#item_select-box1', function(){
+    var selectBox = $('#item_select-box1');
+    var categoryId = $(selectBox).val();
 
-  $.ajax({
-    type: 'GET',
-    url: '/item/category_children',
-    data: {categoryId: categoryId},
-    dataType: 'json'
-  })
-  .done(function(categories){
-    var options = '';
-    $.each(categories.childrens, function(i){
-      category = categories.childrens[i];
-      options += `<option value="${category.id}">${category.name}</option>`;
-    });
-    $("#item_select-box3").remove();
-    $("#item_select-box2").remove();
-    $(selectBox).after(
-      `<select class="item_select-box" id="item_select-box2" name="item[parent_id]">
-        <option value="">---</option>
-        ${options}
-      </select>`
-    );
-  })
+    $.ajax({
+      type: 'GET',
+      url: '/item/category_children',
+      data: {categoryId: categoryId},
+      dataType: 'json'
+    })
+    .done(function(categories){
+      var options = '';
+      $.each(categories.childrens, function(i){
+        category = categories.childrens[i];
+        options += `<option value="${category.id}">${category.name}</option>`;
+      });
+      $("#item_select-box3").remove();
+      $("#item_select-box2").remove();
+      $(selectBox).after(
+        `<select class="item_select-box" id="item_select-box2" name="item[parent_id]">
+          <option value="">---</option>
+          ${options}
+        </select>`
+      );
+    })
+  });
 });
