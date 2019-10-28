a09e8357b76f0c7bfca09dba6e6500b0f73bae20
kei-60th <kei-1005@i.softbank.jp>
2019-10-28 13:21:03 +0900

    画像プレビュー機能turbolinks追加

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,4 +1,4 @@
-$(function(){
+document.addEventListener("turbolinks:load", function() {
   // 「left-bottom__array」内の「img」をマウスオーバーした場合
   $('.left-bottom__array img').hover(function(){
   // マウスオーバーしている画像をメインの画像に反映
