f2d5c614c35bea73c4d447d011c3f3417cd882ba
kei-60th <kei-1005@i.softbank.jp>
2019-11-03 17:25:55 +0900

    [再デプロイ]production.rbのelasticIP変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '3.115.47.67', user: 'ec2-user', roles: %w{app db web}
+server '18.182.209.169', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
