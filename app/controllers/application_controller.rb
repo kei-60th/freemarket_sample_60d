d7dff77610303ab21d312cdf6897fa5183112ab2
KatsunoriGunji <l.hvokj@gmail.com>
2019-11-05 13:04:10 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_category_list
@@ -9,5 +10,17 @@ class ApplicationController < ActionController::Base
 
   def set_category_list
     @category_list = Category.all
+  end
+
+  private
+  
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
